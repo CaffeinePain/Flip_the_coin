41e68170799dd01bd2a5fd33e3817946c8e66e6c
CaffeinePain <whwowns1021@gmail.com>
2021-12-24 23:54:55 +0900

    Complete?

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,15 @@
 const btn = document.querySelector("#btn");
+const showBtn = document.querySelector("#showBtn");
+const rstBtn = document.querySelector("#rstBtn");
+const feedbackLog = document.querySelector("#feedbackLog");
+const sonDeung = document.querySelector("#SonDeung");
 
 let nowCoinExist = false;
+let flipStack = 0;
 
 btn.addEventListener("click", flip);
+showBtn.addEventListener("click", showResult);
+rstBtn.addEventListener("click", reset);
 
 function flip() {
     if (nowCoinExist == true) {
@@ -13,16 +20,34 @@ function flip() {
     coin.id = "coinImg";
     if (randnum < 0.5) {
         coin.src = "Image/Head.png";
-        console.log("Head!");
+        //console.log("Head!");
     } else{
         coin.src = "Image/Tail.png";
-        console.log("Tail!");
+        //console.log("Tail!");
     }
     document.body.appendChild(coin);
     nowCoinExist = true;
+    announceFlipStack();
 }
 
 function resetImg() {
     const coin = document.querySelector("#coinImg");
     coin.remove();
+}
+
+function announceFlipStack() {
+    flipStack++;
+    feedbackLog.innerText = `${flipStack} Flip!`;
+}
+
+function showResult() {
+    sonDeung.style.transform = "rotate(90deg)";
+}
+
+function reset() {
+    flipStack = 0;
+    feedbackLog.innerText = "";
+    resetImg();
+    nowCoinExist = false;
+    sonDeung.style.transform = "translateX(-50%)";
 }
